73b71434c547198cecb7c3f85186393a5348a13c
julio hernandez <julioandres72@gmail.com>
2020-08-22 16:51:14 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,10 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def valid_move?(board, position)
 	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
+	if position_taken?(board, spot) && spot.between?(0,8)
 		true
 	else
 		false
-
 	end
 end
 
